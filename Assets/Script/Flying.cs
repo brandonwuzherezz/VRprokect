24558ec0a8181bc5b32f318bc2678547ccbaf6a4
brandonwuzherezz <bsamudio@ucsc.edu>
2018-08-17 00:43:54 -0700

    NewStuff

---
+++ Assets/Script/Flying.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour {
+
+	public bool flying = true;
+	private Vector3 spawnPoint;
+
+	void Start(){
+		spawnPoint = transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		while (flying == true) {
+			transform.position = transform.position + Camera.main.transform.forward * 5f * Time.deltaTime;
+		}
+		///
+		if (transform.position.x > 125f) {
+			transform.position = spawnPoint;
+		}
+		if (transform.position.x < -125f) {
+			transform.position = spawnPoint;
+		}
+		if (transform.position.y > 150f) {
+			transform.position = spawnPoint;
+		}
+		if (transform.position.y < 0f) {
+			transform.position = spawnPoint;
+		}
+		if (transform.position.z > 120f) {
+			transform.position = spawnPoint;
+		}
+		if (transform.position.z < -120f) {
+			transform.position = spawnPoint;
+		}
+	}
+}
